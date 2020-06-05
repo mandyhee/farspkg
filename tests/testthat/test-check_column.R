342cfca416bc1b1826924e87cccd55117e731de1
mandyhee <homengni0316@gmail.com>
2020-06-05 14:12:19 -0700

    update

--- tests/testthat/test-check_column.R
+++ tests/testthat/test-check_column.R
@@ -1,5 +1,4 @@
-# test number of columns are 50
-test_that("test number of columes are 50",{
+test_that("test number of columes are 50", {
   file = paste0(system.file("extdata", package = "farspkg"), "/",make_filename(2013))
   expect_equal(ncol(fars_read(file)), 50)
 })
